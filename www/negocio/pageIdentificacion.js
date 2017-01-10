430746ff533a6049ee99ba2f8cc36191b202bb40
settingcons <settingcons@gmail.com>
2017-01-10 15:36:33 +0100

    vtest

--- www/negocio/pageIdentificacion.js
+++ www/negocio/pageIdentificacion.js
@@ -10,7 +10,6 @@ function inicioPaginaIdentificacion() {
 
 function cargaDatosCiudadano(){
 
-
     LimpiarDatosCiudadano();
 
     var objUsu = getDatosUsuario();
@@ -30,10 +29,8 @@ function cargaDatosCiudadano(){
         $('#inputDNI').val(objUsu.DNI);
         $('#inputEMAIL').val(objUsu.EMAIL);
         $('#inputTELEFON').val(objUsu.TELEFON);
-
     }
 }
-
 
 function guardaDatosCiudadano(){
     try
@@ -78,7 +75,6 @@ function guardaDatosCiudadano(){
 function ComprobarUsuarioWS(sParams){
     alert ("HGS sParams: " + sParams.p_sNom + ","+sParams.p_sCognom1+ ","+sParams.p_sCognom2+ ","+sParams.p_sDni+ ","+sParams.p_sDni+ ","+sParams.p_sEmail+ ","+sParams.p_sTelefon+ ".")
     alert ("url: "+ _wsURLLogin);
-    alert("datos: " + datos);
     /*url: _wsURLLogin -->'http://172.26.0.2:8000/wsAPPGIV/wsIncidentNotifierGIV.asmx/Login_v1'*/
     var url = "http://172.26.0.2:8000/wsAPPGIV/wsIncidentNotifierGIV.asmx/";
     try {
@@ -86,7 +82,7 @@ function ComprobarUsuarioWS(sParams){
             type: "POST",
             url:url+"Login_v1",
             data: sParams,
-            success: function (ComprobarUsuarioWS_OK){alert("OK");},
+            success: function (ComprobarUsuarioWS_OK){alert("OK"); alert("datos: " + datos);},
             error: function(ComprobarUsuarioWS_ERROR){alert("KO");},
             async:false
         });
