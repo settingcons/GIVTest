30993f5cda7a3ec6a82a1a65a0eb2974ec4dc245
settingcons <settingcons@gmail.com>
2017-01-10 14:03:58 +0100

    vtest
    messs

--- www/negocio/pageIdentificacion.js
+++ www/negocio/pageIdentificacion.js
@@ -78,13 +78,13 @@ function guardaDatosCiudadano(){
 function ComprobarUsuarioWS(sParams){
     alert ("HGS sParams: " + sParams.p_sNom + ","+sParams.p_sCognom1+ ","+sParams.p_sCognom2+ ","+sParams.p_sDni+ ","+sParams.p_sDni+ ","+sParams.p_sEmail+ ","+sParams.p_sTelefon+ ".")
     alert ("url: "+ _wsURLLogin);
-    /*url: _wsURLLogin,*/
+    /*url: _wsURLLogin -->'http://172.26.0.2:8000/wsAPPGIV/wsIncidentNotifierGIV.asmx/Login_v1'*/
     try {
         $.ajax({
-            type: 'POST',
-            url:'http:\\172.26.0.2:8000\wsAPPGIV\wsIncidentNotifierGIV.asmx\Login_v1',
+            type: "POST",
+            url:"http://172.26.0.2:8000/wsAPPGIV/wsIncidentNotifierGIV.asmx?op=Login_v1",
             data: sParams,
-            dataType:'text',
+            dataType:"text",
             success: ComprobarUsuarioWS_OK,
             error: ComprobarUsuarioWS_ERROR,
             async:false
